d6d661d99208bd65af2c6c4d56ef08de7e082e21
AnikaRahman09 <antaraanika28@gmail.com>
2022-11-09 22:40:00 +0600

    express js practice

---
+++ data/blogs.js
@@ -0,0 +1,29 @@
+blogs = [
+
+    {
+        title: "How to get started with Python",
+        content: "Python content here",
+        slug: "python-learn"
+    },
+
+    {
+        title: "How to get started with Js",
+        content: "Js content here",
+        slug: "js-learn"
+    },
+
+    {
+        title: "How to get started with Django",
+        content: "Django content here",
+        slug: "django-learn"
+    },
+
+    {
+        title: "How to get started with CSS",
+        content: "CSS content here",
+        slug: "css-learn"
+    }
+]
+        
+    
+module.exports = blogs;
